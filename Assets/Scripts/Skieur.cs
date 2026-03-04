e28a4d58c3c462bc5fbe37f73a1c94a809d94b1b
Nathaniel Ouellet <e2555551@cmaisonneuve.qc.ca>
2026-03-04 14:51:47 -0500

    Mouvement du Skieur

--- Assets/Scripts/Skieur.cs
+++ Assets/Scripts/Skieur.cs
@@ -1,28 +1,43 @@
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.InputSystem;
 
 public class Skieur : MonoBehaviour
 {
-    void Start()
-    {
+    public float vitesseDeplacementHorizontal = 1f;
+    public float vitesseDeplacementVertical = 1f;
+    public InputAction actionMovementHorizontal;
+    public InputAction actionMovementVertical;
+    private Rigidbody2D rb;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Utiliser ces fonctions pour activer et désactiver les InputActions
+    void Update()
+    {
+        float vitesseHorizontale = actionMovementHorizontal.ReadValue<float>();
+        float vitesseVerticale = actionMovementVertical.ReadValue<float>();
+        rb.AddForceY(0f - (vitesseVerticale * vitesseDeplacementVertical * Time.deltaTime));
+        transform.Translate(vitesseDeplacementHorizontal * vitesseHorizontale * Time.deltaTime * (((0f - rb.linearVelocityY) / 5f) + 1f), 0f, 0f, Space.Self);
+    }
+
     private void OnEnable()
     {
-
+        actionMovementHorizontal.Enable();
+        actionMovementVertical.Enable();
     }
 
     private void OnDisable()
     {
-
+        actionMovementHorizontal.Disable();
+        actionMovementVertical.Disable();
     }
 
 
-
     // Il faut appeller cette fonction dans la collision avec le yéti.
-    void DeconnecterCamera()
+    public void DeconnecterCamera()
     {
         Camera.main.GetComponent<PositionConstraint>().enabled = false;
     }
